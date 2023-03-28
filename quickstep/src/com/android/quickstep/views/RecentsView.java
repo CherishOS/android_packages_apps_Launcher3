f8103a60cc2e3c35a0195b46280a8ad80359c5c8
Pranav Vashi <neobuddy89@gmail.com>
2023-03-28 20:56:22 +0530

    Reland 'Launcher3: Fix blurry recents issue with live tile'
    * Issue description: https://github.com/LawnchairLauncher/lawnchair/issues/2762
    
    mnri: this reduces the occurences of the taskview getting stucked after swipe up gesture
    
    Change-Id: I363f4ace8b2bde2bc4ed6e6f7f608b850e0359f8
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- quickstep/src/com/android/quickstep/views/RecentsView.java
+++ quickstep/src/com/android/quickstep/views/RecentsView.java
@@ -2541,6 +2541,7 @@ public abstract class RecentsView<ACTIVITY_TYPE extends StatefulActivity<STATE_T
 
         setEnableFreeScroll(true);
         setEnableDrawingLiveTile(mCurrentGestureEndTarget == GestureState.GestureEndTarget.RECENTS);
+        setRunningTaskViewShowScreenshot(true);
         setRunningTaskHidden(false);
         animateUpTaskIconScale();
         animateActionsViewIn();
