3a316d0a27b9cc12a39abb5a58d4021236b86a5d
minaripenguin <minaripenguin@users.noreply.github.com>
2023-05-01 22:22:46 +0800

    Launcher3: Always show all apps label instead of line
    * matches pixel launcher
    
    Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- quickstep/src/com/android/launcher3/appprediction/AppsDividerView.java
+++ quickstep/src/com/android/launcher3/appprediction/AppsDividerView.java
@@ -144,10 +144,8 @@ public class AppsDividerView extends View implements FloatingHeaderRow {
                 }
             }
 
-            if (mShowAllAppsLabel && sectionCount > 0) {
+            if (sectionCount > 0) {
                 dividerType = DividerType.ALL_APPS_LABEL;
-            } else if (sectionCount == 1) {
-                dividerType = DividerType.LINE;
             } else {
                 dividerType = DividerType.NONE;
             }
