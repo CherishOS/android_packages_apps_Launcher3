3a1dec9b4baf29b7b4b141d3475043cf70936a5e
Fuqiang Xie <ot_fuqiang.xie@mediatek.com>
2025-02-19 09:48:56 +0800

    Fix Navbar height issue
    When used GMS load, the first time phone boot, the navbar height was abnormal during the setup wizard.
    The reason is that when the phone boot, the height of the gesture layout is used.
    Therefore, when using the three-buttons navigation layout, do not use the height of the gesture layout.
    
    Bug: 397550729
    Test: manual
          test device boot.
    
    Change-Id: I6e7aaf279a410d03f6f0068f26a4e341673c4334
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Hưng Phan <phandinhhungvp2001@gmail.com>

--- quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
+++ quickstep/src/com/android/launcher3/taskbar/NavbarButtonsViewController.java
@@ -302,7 +302,7 @@ public class NavbarButtonsViewController implements TaskbarControllers.LoggableT
                 mContext.isGestureNav());
         ViewGroup.LayoutParams navButtonsViewLayoutParams = mNavButtonsView.getLayoutParams();
         navButtonsViewLayoutParams.width = p.x;
-        if (!mContext.isUserSetupComplete()) {
+        if (!mContext.isUserSetupComplete() && !isThreeButtonNav) {
             // Setup mode in phone mode uses gesture nav.
             navButtonsViewLayoutParams.height = setupSize;
         } else {
